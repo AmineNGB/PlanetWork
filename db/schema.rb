bf497f6dc06f7001211eeb9c010e15cf78f1e08b
Amine <amineneghbel@gmail.com>
2020-02-24 16:18:55 +0100

    Controller Planets created with routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 202002241513014) do
+ActiveRecord::Schema.define(version: 202002241513015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 202002241513014) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_planets_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "planet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["planet_id"], name: "index_reservations_on_planet_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +48,6 @@ ActiveRecord::Schema.define(version: 202002241513014) do
   end
 
   add_foreign_key "planets", "users"
+  add_foreign_key "reservations", "planets"
+  add_foreign_key "reservations", "users"
 end
