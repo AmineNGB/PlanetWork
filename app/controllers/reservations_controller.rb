83de0820ad6b6201711b597107ef8bfa44015d0e
Amine <amineneghbel@gmail.com>
2020-02-24 18:18:37 +0100

    Models validations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,5 @@
 class ReservationsController < ApplicationController
   def user_show
     @reservations = Reservation.where(user: current_user)
-
-end
-
-
+  end
 end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,3 +1,7 @@
 class Planet < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
