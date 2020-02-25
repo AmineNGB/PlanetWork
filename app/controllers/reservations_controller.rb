ab40d9495537171b909c44957b0ffeaa2c48ab26
jules le morvan jules.beauterra^gmail.com <jules.beauterra@gmail.com>
2020-02-25 13:11:31 +0100

    create reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,4 +3,8 @@ class ReservationsController < ApplicationController
   def user_show
     @reservations = Reservation.where(user: current_user)
   end
+
+  def date
+    date = Date.civil(*params[:event].sort.map(&:last).map(&:to_i))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'planets#index'
 
-  resources :planets
+  resources :planets do
+    resources :reservations
+  end
 
   get '/reservations', to: 'reservations#user_show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
